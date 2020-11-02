c06b5bdb12c66d109578e30df504894b426143a7
{Garrett <{garrett.w.knowles@ou.edu}>
2020-11-02 17:02:38 -0600

    Repo instructions from chat added

--- src/SuperStack.java
+++ src/SuperStack.java
@@ -4,6 +4,14 @@ public class SuperStack {
 	public SuperStack() {
 		
 	}
+	
+	//For all of those having errors:
+		//1. remove .git
+		//2. git init
+		//3. git add .
+		//4. git commit -m "init"
+		//5. follow instructions on GitHub empty repo (git origin..., git branch..., git push...)
+
 	public void peek() {
 		
 	}
